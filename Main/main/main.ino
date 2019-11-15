460ca55124cfbd931d7ce5f5d881a27b8b81dc6a
Laurensjkok <56067684+Laurensjkok@users.noreply.github.com>
2019-11-15 10:26:04 +0100

    Update main.ino
    Included code

--- Main/main/main.ino
+++ Main/main/main.ino
@@ -1,9 +1,51 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <NewPing.h>
+#include <Servo.h>
 
+#define TRIGGER_PIN  9
+#define ECHO_PIN     9
+#define MAX_DISTANCE 200
+
+Servo servoLeft;
+Servo servoRight;
+
+const int rightServoPin = 13;       
+const int leftServoPin = 12;
+
+void move_servos(int speed_left, int speed_right)
+{
+    servoLeft.write(180 - speed_left);
+    servoRight.write(speed_right);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup() 
+{
+  Serial.begin(9600);
+  servoLeft.attach(leftServoPin);
+  servoRight.attach(rightServoPin);
+  
+  // Stop the servo motors
+  servoLeft.write(90);
+  servoRight.write(90);
+}
 
+void loop()
+{
+  int ThrottleR, ThrottleL;
+  //Start driving (Max throttle is 180, 90 is standstill)
+  ThrottleL = 180;
+  ThrottleR = 180;
+  
+  //if right sensor detects line, steer left
+  //ThrottleL = ThrottleL-5;
+
+  //if left sensor detects line, steer right
+  //THrottleR = ThrottleR-5;
+
+  //if both sensors detect line, drive straight (do nothing)
+
+  //if both sensors do not detect a line, drive straight (do nothing)
+
+  move_servos(ThrottleL, ThrottleR);
+  delay(1000);
+  
 }
