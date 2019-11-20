4ae8c1b18bb2941832c29708bafc8a2ad7bd4266
Mitchell van Es <mitchellvanes@gmail.com>
2019-11-20 14:59:04 +0100

    Update main.ino
    states test

--- Main/main/main.ino
+++ Main/main/main.ino
@@ -21,16 +21,24 @@ int rightThreshold;
 
 const float alpha = 0.005;
 unsigned long StartTime = 0;
-float baseSpeed = 0.05;
+float baseSpeed = -0.05;
+
+enum States{
+	left,
+	right,
+	none
+};
+
+enum States state = none;
 
 void move_servos(float baseSpeed, float offset)
 {
 
-  float speed_left = baseSpeed +  0.07*offset + 0.10*offset^2;
-  float speed_right = -baseSpeed + 0.07*offset + 0.10*offset^2;
+	float speed_left = baseSpeed +  offset;
+	float speed_right = -baseSpeed + offset;
 
-  speed_left = constrain(speed_left, -1, 1);
-  speed_right = constrain(speed_right, -1, 1);
+	speed_left = constrain(speed_left, -1, 1);
+	speed_right = constrain(speed_right, -1, 1);
       
     servo_left.write(90+speed_left*90);
     servo_right.write(90+speed_right*90);
@@ -38,61 +46,64 @@ void move_servos(float baseSpeed, float offset)
 
 void setup() 
 {
-  Serial.begin(9600);
-  servo_left.attach(pin_Servo_left);
-  servo_right.attach(pin_Servo_right);
+	Serial.begin(9600);
+	servo_left.attach(pin_Servo_left);
+	servo_right.attach(pin_Servo_right);
 
-  IR_left = analogRead(pin_IR_left);
-  IR_right = analogRead(pin_IR_right);
+	IR_left = analogRead(pin_IR_left);
+	IR_right = analogRead(pin_IR_right);
 
-  leftThreshold = IR_left + 50;
-  rightThreshold = IR_right + 50;
+	leftThreshold = IR_left + 50;
+	rightThreshold = IR_right + 50;
   
   // Init servo motors with 0
-  servo_right.write(90);
-  servo_right.write(90);
+	servo_right.write(90);
+	servo_right.write(90);
 }
 
 void loop()
 {    
   // Read from IR sensors
     IR_left = analogRead(pin_IR_left);
-    IR_right = analogRead(pin_IR_right);
-
-  
+    IR_right = analogRead(pin_IR_right); 
     
     if(IR_left < leftThreshold && IR_right < rightThreshold){
     // If no line is detected
-    StartTime = 0;
-    move_servos(baseSpeed, 0);
-    
+		StartTime = 0;
+		move_servos(baseSpeed, 0);
+    	state = none;
+		
     }else if (IR_left > leftThreshold && IR_right < rightThreshold) {
     // if line is detected by left side
-    
+
     // if StartTime is not set set it
-    if(!StartTime){
-      StartTime = millis();
-    }
-    
-      move_servos(baseSpeed, -alpha*(millis() - StartTime));
-  
+		if(!StartTime){
+			StartTime = millis();
+		}
+		if(state != right){
+			move_servos(baseSpeed, -alpha*(millis() - StartTime));
+		}
+		state = left;
     
     }else if (IR_left < leftThreshold && IR_right > rightThreshold) {
     // if line is detected by right side
-    
+		
     // if StartTime is not set set it
-    if(!StartTime){
-      StartTime = millis();
-    }
-
-      move_servos(baseSpeed, alpha*(millis() - StartTime));
- 
+		if(!StartTime){
+			StartTime = millis();
+		}
+		if(state != left){
+			move_servos(baseSpeed, alpha*(millis() - StartTime));
+		}
+ 		state = right;
     
     }else if(IR_left > leftThreshold && IR_right > rightThreshold){
     // if both detect a line (consider it as no line for now)
     
-    StartTime = 0;
-    move_servos(baseSpeed, 0);
+		StartTime = 0;
+		if(state == none){
+			move_servos(baseSpeed, 0);
+		}
       // Intersection protocol
     }
     Serial.print("Left sensor value: ");
