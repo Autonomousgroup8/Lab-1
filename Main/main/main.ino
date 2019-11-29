7c3c88c4e9ac82b98197bdb0cbc669ce20be5b13
JobvanderWorp <jobvanderworp@gmail.com>
2019-11-29 16:27:19 +0100

    Update
    Onnodige variable eruit gehaald en code netter gemaakt

--- Main/main/main.ino
+++ Main/main/main.ino
@@ -24,17 +24,16 @@ int IR_right = 0;
 int turnleft = 0;
 int turnright = 0;
 int rechtdoor = 0;
-int trash = 0;
 int prevCross = 0;
 const float alpha = 0.3;
 const float beta = 0.002;
-const float gamma = 10;
-unsigned long StartTime = 0;
+const float gamma = 50;
 float baseSpeed = 0.05;
 float ACCSpeed = 0.05;
 const int FilterLength = 10;
 int TurningAverage[FilterLength];
 int FilterIndex = 0;
+
 //Zigbee implementation
 #define SELF     43
 #define PAN_ID           "A008"
@@ -46,13 +45,6 @@ int crossingsPassed = 0;
 #define STRING(name) #name
 #define TOSTRING(x) STRING(x)
 
-// the xbee_init function initializes the XBee Zigbee module
-// When the program is *running*, the switch on the wireless proto shield should be in the position 'MICRO'.
-// During programming of the Arduino, the switch should be in the position 'USB'.
-// It will only work if the XBee module is set to communicate at 9600 baud. If it is not, the module needs to be reprogrammed
-// on the USB XBee dongle using the XCTU program.
-
-
 float Average(int Array[], int length) {          //floating average over 10 cycles
   int sum = 0;
   for (int i = 0; i < length; i++) {              //sum all elements of Array
@@ -61,6 +53,8 @@ float Average(int Array[], int length) {          //floating average over 10 cyc
 
   return sum / length;                            //calculate average of Array
 }
+
+// the xbee_init function initializes the XBee Zigbee module
 void xbee_init(void) {
   Serial.begin(9600);                         // set the baud rate to 9600 to match the baud rate of the xbee module
   Serial.flush();                             // make sure the buffer of the serial connection is empty
@@ -73,10 +67,9 @@ void xbee_init(void) {
   Serial.print("ATCN\r");                     // exit command mode and return to transparent mode, communicate all data on the serial link onto the wireless network
 }
 
-void move_servos(float baseSpeed, float offset) {  //mpve servos
+void move_servos(float baseSpeed, float offset) {
   float speed_left = -baseSpeed +  offset;
   float speed_right = baseSpeed + offset;
-
 
   speed_left = constrain(speed_left, -1, 1);
   speed_right = constrain(speed_right, -1, 1);
@@ -90,7 +83,7 @@ void setup()
   xbee_init();
   Serial.begin(9600);
   Serial.println("This is the XBee - Broadcast program.");
-  servo_left.attach(pin_Servo_left);              //attach all servos
+  servo_left.attach(pin_Servo_left);
   servo_right.attach(pin_Servo_right);
   servo_head.attach(pin_Servo_head);
   servo_left.write(90);
@@ -99,7 +92,7 @@ void setup()
 
 float ACC() {                                                   //active cruise control algorithm, determines speed
   int Distance = sonar.ping_cm();
-//  Serial.println(Distance);
+  //  Serial.println(Distance);
   if (Distance > 49 || Distance == 0) {                         // Nothing in front, you are the leader
     return ACCSpeed;
   } else if (Distance < 50 && Distance > Ref_Distance + 5) {    // Larger than preffered distance, gradruately speed up
@@ -115,60 +108,32 @@ void loop()
 {
   if (!waitMode) {
     baseSpeed = ACC();                          //determine speed with Active cruise control.
-//    Serial.print(baseSpeed);
+    //Serial.print(baseSpeed);
+
     // Read from IR sensors
     IR_left = digitalRead(pin_IR_left);
     IR_right = digitalRead(pin_IR_right);
 
-//    if (IR_right == LOW && turnright > 6) {     //sharp corner protocol right
-//      turnright = 0;
-//      move_servos(baseSpeed, 1);
-//      delay(100);
-//      while (IR_left == HIGH) {
-//        move_servos(baseSpeed, 0);
-//        IR_left = digitalRead(pin_IR_left);
-//      }
-//    }
-//    if (IR_left == LOW && turnleft > 6) {     //sharp corner protocol right
-//      turnleft = 0;
-//      move_servos(baseSpeed, -alpha);
-//      delay(100);
-//      while (IR_right == HIGH) {
-//        move_servos(baseSpeed, 0);
-//        IR_right = digitalRead(pin_IR_right);
-//      }
-//    }
-//
-//    if (IR_right == LOW) {                    //reset consequetive right turns counter
-//      turnright = 0;
-//    }
-//    if (IR_left == LOW) {                    //reset consequetive left turns counter
-//      turnleft = 0;
-//    }
-
     if (IR_left == LOW && IR_right == LOW) {      // If no line is detected
-      move_servos(baseSpeed, 0);
       rechtdoor++;
       if (prevCross == 1) {
         crossingsPassed++;
         prevCross = 0;
       }
-      if (crossingsPassed > 2) {
-        Serial.print(6);
-        crossingsPassed = 0;
-      }
+
       if (rechtdoor > 20) {                       //increase speed on long straights
         move_servos(2 * baseSpeed, 0);
+      } else {
+        move_servos(baseSpeed, 0);
       }
       TurningAverage[FilterIndex] = 0;           //update average for head direction
-    }
-    else if (IR_left == HIGH && IR_right == LOW) { // if line is detected by left side
+    } else if (IR_left == HIGH && IR_right == LOW) { // if line is detected by left side
       turnleft ++;
       rechtdoor = 0;
       if (turnleft > 10) {                         //on sharp corners turn faster
         move_servos(baseSpeed, -2 * alpha);
-      }else{
-      move_servos(baseSpeed, -alpha);
+      } else {
+        move_servos(baseSpeed, -alpha);
       }
       TurningAverage[FilterIndex] = -1;           //update average for head direction
 
@@ -177,27 +142,17 @@ void loop()
       rechtdoor = 0;
       if (turnright > 10) {                         //on sharp corners turn faster
         move_servos(baseSpeed, 2 * alpha);
-      }else{
-      move_servos(baseSpeed, alpha);
-      }      
+      } else {
+        move_servos(baseSpeed, alpha);
+      }
       TurningAverage[FilterIndex] = 1;           //update average for head direction
-    } else if (IR_left == HIGH && IR_right == HIGH && crossingsPassed == 0) {
+    } else if (IR_left == HIGH && IR_right == HIGH) {
       rechtdoor = 0;
       waitMode = true;
       prevCross = 1;
       move_servos(0, 0);
-      Serial.print(6);
-      trash = Serial.read();
     }
-    else if (IR_left == HIGH && IR_right == HIGH && crossingsPassed > 0) {
-      // if both detect a line (consider it as no line for now)
-      rechtdoor = 0;
-      prevCross = 1;
-      move_servos(baseSpeed, 0);
-    }
-  }
-  else if (waitMode == true) {
-    //Serial.println("IK WACHT");
+  } else if (waitMode == true) {
     if (Serial.available() > 0) {
       incomingByte = Serial.read();
       if (incomingByte == 55) {
